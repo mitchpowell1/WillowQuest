c89564a6ac79fca9b0db1834572bb70d954df58f
mitchpowell1 <mdp9648@uncw.edu>
2016-08-20 12:03:16 -0400

    First Commit

---
+++ src/Cell.java
@@ -0,0 +1,15 @@
+/***
+ * The Cell enumerable type is used to label the type of each cell in a dungeon
+ * @author Mitch Powell
+ *
+ */
+public enum Cell {
+	CORRIDOR,
+	DOOR,
+	ENTRANCE,
+	EXIT,
+	ROOM,
+	TRAP,
+	TREASURE,
+	WALL
+}
