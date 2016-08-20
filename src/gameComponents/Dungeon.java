20f9886baf809a0c2b5a9f31162336ab8b4a6718
mitchpowell1 <mdp9648@uncw.edu>
2016-08-20 12:23:32 -0400

    Did some refactoring and removed some misplaces elements from the DungeonFactory class

--- src/gameComponents/Dungeon.java
+++ src/gameComponents/Dungeon.java
@@ -1,22 +1,49 @@
 package gameComponents;
 
+/***
+ * Dungeon object definition. A dungeon primarily houses an MxN array of cells.
+ * @author mitch
+ *
+ */
 public class Dungeon {
 	
 	private Cell [][] cells;
 	private int width;
 	private int height;
 	
+	/***
+	 * The dungeon constructor class accepts a 2D array of Cells as input and
+	 * sets the height and width attributes as a derivation of the size of the
+	 * Cell array
+	 * @param newCells
+	 */
 	public Dungeon(Cell [][] newCells){
 		this.cells = newCells;
 		this.width = newCells[0].length;
 		this.height = newCells.length;
 	}
 	
+	/***
+	 * Accesser method for the width of the dungeon.
+	 * @return the width of the dungeon in cells
+	 */
 	public int getWidth(){
 		return this.width;
 	}
 	
+	/***
+	 * Accessor method for the height of the dungeon
+	 * @return the height of the dungeon in cells.
+	 */
 	public int getHeight(){
 		return this.height;
+	}
+	
+	/***
+	 * Accessor method for the dungeon's cell array.
+	 * @return the array of cells in a dungeon.
+	 */
+	public Cell[][] getCells(){
+		return this.cells;
 	}
 }
---
+++ src/gameComponents/DungeonFactory.java
@@ -0,0 +1,8 @@
+package gameComponents;
+public class DungeonFactory {
+	/***
+	 * Constructor method for a dungeon generator object
+	 */
+	public DungeonFactory(){
+	}
+}
